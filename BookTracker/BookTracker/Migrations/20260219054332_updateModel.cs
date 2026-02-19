3dcaf21f1373d3beeebef2a468abebee18300bb6
idekferewf <example@gmail.com>
2026-02-19 09:11:05 +0300

    Обновил модель и сделал новую миграцию

---
+++ BookTracker/BookTracker/Migrations/20260219054332_updateModel.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Books");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GenreId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Genres_GenreId",
+                table: "Books",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Genres_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- BookTracker/BookTracker/Model/Book.cs
+++ BookTracker/BookTracker/Model/Book.cs
@@ -3,9 +3,9 @@
     public class Book : EFModel
     {
         public string? Title { get; set; }
-        public string? Author { get; set; }
+        public Author Author { get; set; }
         public int Year { get; set; }
-        public string? Genre { get; set; }
+        public Genre Genre { get; set; }
         public string? Notes {  get; set; }
         public DateTime AddDateTime {  get; set; }
         public enum Status {Прочитано, Читаю, В_планах, Отложено }
